b8c06f81bffa8f570defc335bafa29c62f990d88
goandreus <andres.chavez.crisostomo@gmail.com>
2020-07-31 17:09:55 -0500

    add first model

---
+++ lib/models/person_model.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class PersonModel{
+  final IconData iconoIzquierda;
+  final IconData iconoDerecha;
+  final String nombre;
+  final String descripcion;
+
+  PersonModel({this.iconoIzquierda, this.iconoDerecha, this.nombre, this.descripcion});
+  
+}
