80df1d76cb788f8bad17e3adab762346b6f74d8f
goandreus <andres.chavez.crisostomo@gmail.com>
2020-08-01 18:27:47 -0500

    value notifier

--- lib/ui/widgets/custom_buttons.dart
+++ lib/ui/widgets/custom_buttons.dart
@@ -2,44 +2,40 @@ import 'package:flutter/material.dart';
 import 'package:todo_expo/controllers/main_controller.dart';
 import 'package:todo_expo/ui/pages/form_contact_page.dart';
 
-class CustomButton extends StatefulWidget {
-  @override
-  _CustomButtonState createState() => _CustomButtonState();
-}
-
-class _CustomButtonState extends State<CustomButton> {
-  String titulo;
-
-  @override
-  void initState() {
-    super.initState();
-    titulo = 'Add contact';
-  }
+class CustomButton extends StatelessWidget {
+  
+  ValueNotifier<String> titulo = ValueNotifier<String>('Add Contact');
 
   @override
   Widget build(BuildContext context) {
     return InkWell(
       onTap: () {
+        titulo.value = "Value notifier button";
         pressButton();
         // setState(() {
         //   titulo = 'Tittle Button';
         // });
-        Navigator.pushNamed(context, 'form_contact');
+        //Navigator.pushNamed(context, 'form_contact');
       },
-      child: Container(
-        height: 60,
-        width: double.infinity,
-        margin: EdgeInsets.all(8),
-        padding: EdgeInsets.all(8),
-        decoration: BoxDecoration(
-            border: Border.all(color: Theme.of(context).primaryColor),
-            borderRadius: BorderRadius.circular(10)),
-        child: Center(
-          child: Text(
-            titulo,
-            style: TextStyle(color: Theme.of(context).primaryColor, fontSize: 20),
+      child: ValueListenableBuilder(
+        valueListenable: titulo,
+        builder: (context,value,child){
+          return Container(
+          height: 60,
+          width: double.infinity,
+          margin: EdgeInsets.all(8),
+          padding: EdgeInsets.all(8),
+          decoration: BoxDecoration(
+              border: Border.all(color: Theme.of(context).primaryColor),
+              borderRadius: BorderRadius.circular(10)),
+          child: Center(
+            child: Text(
+              value,
+              style: TextStyle(color: Theme.of(context).primaryColor, fontSize: 20),
+            ),
           ),
-        ),
+        );
+        },
       ),
     );
   }
