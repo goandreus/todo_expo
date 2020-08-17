8e4b662acb233d91e7bf40cc4e3601d7781ca8eb
goandreus <andres.chavez.crisostomo@gmail.com>
2020-08-16 19:29:39 -0500

    add responsive

---
+++ lib/utils/responsive.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/widgets.dart';
+import 'package:meta/meta.dart' show required;
+import 'dart:math' as math;
+
+class Responsive {
+  final double width, height, inch;
+  
+  Responsive({@required this.width,@required this.height,@required this.inch});
+
+  factory Responsive.of(BuildContext context){
+    final MediaQueryData data = MediaQuery.of(context);
+    final size = data.size;
+
+    final inch = math.sqrt(math.pow(size.width, 2) + math.pow(size.height, 2));
+    return Responsive(height: size.height, width: size.width, inch: inch);
+  }
+
+  //width
+  double wp(double percent){
+    return this.width* percent / 100;
+  }
+
+  //height
+  double hp(double percent){
+    return this.height* percent / 100;
+  }
+
+  //diagonal
+  double ip(double percent){
+    return this.inch* percent / 100;
+  }
+}
